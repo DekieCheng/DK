28d41843a8e790e5d513368f159201eb37b99137
Dekie Cheng <dekie.cheng@flex.com>
2021-07-26 14:36:18 +0800

    Removed the obj folder

--- PrintLabelSimulator/PrintLabelSimulator/PrintLabelSimulator/My Project/AssemblyInfo.vb
+++ PrintLabelSimulator/PrintLabelSimulator/PrintLabelSimulator/My Project/AssemblyInfo.vb
@@ -8,10 +8,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("PrintLabelSimulator")>
+<Assembly: AssemblyTitle("Print Label Simulator")>
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("PrintLabelSimulator")>
+<Assembly: AssemblyProduct("Print Label Simulator")>
 <Assembly: AssemblyCopyright("Copyright ©  2021")>
 <Assembly: AssemblyTrademark("")>
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.0.101")>
+<Assembly: AssemblyFileVersion("1.0.0.101")>
